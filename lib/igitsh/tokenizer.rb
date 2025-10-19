2f6699c36a6c91b9278dabd6738382524c957b37
apainintheneck <apainintheneck@gmail.com>
2025-10-18 17:45:40 -0700

    Clean up Tokenizer class

--- lib/igitsh/tokenizer.rb
+++ lib/igitsh/tokenizer.rb
@@ -4,11 +4,6 @@ require "strscan"
 
 module Igitsh
   module Tokenizer
-    SINGLE_QUOTE = "'"
-    DOUBLE_QUOTE = '"'
-    BACKSLASH = "\\"
-    private_constant :SINGLE_QUOTE, :DOUBLE_QUOTE, :BACKSLASH
-
     # @param line [String]
     #
     # @return [Igitsh::TokenZipper]
@@ -52,83 +47,44 @@ module Igitsh
             start_position: start_position,
             end_position: scanner.charpos - 1
           )
-        else # quoted or unquoted string
-          tokens << scan_string_token(
-            line: line,
+        elsif scanner.skip(/'(?:\\.|[^'])*/) # single-quoted string
+          tokens << if scanner.skip("'")
+            Token::String
+          else
+            Token::UnterminatedString
+          end.new(
+            source: line,
             start_position: start_position,
-            scanner: scanner
+            end_position: scanner.charpos
           )
+        elsif scanner.skip(/"(?:\\.|[^"])*/) # double-quoted string
+          tokens << if scanner.skip('"')
+            Token::String
+          else
+            Token::UnterminatedString
+          end.new(
+            source: line,
+            start_position: start_position,
+            end_position: scanner.charpos
+          )
+        elsif scanner.skip(/[^&|;'"\s]+/) # unquoted string identifier
+          # Everything else that is not:
+          # - an ampersand
+          # - a pipe character
+          # - a semicolon
+          # - a single or double quote
+          # - a whitespace character
+          tokens << Token::String.new(
+            source: line,
+            start_position: start_position,
+            end_position: scanner.charpos
+          )
+        else
+          raise UnreachableError, "#{start_position}: Unknown string parsing error"
         end
       end
 
       TokenZipper.new(source: line, tokens: tokens.freeze)
     end
-
-    # @param line [String]
-    # @param start_position [Integer]
-    # @param scanner [StringScanner]
-    #
-    # @return [String]
-    def self.scan_string_token(line:, start_position:, scanner:)
-      terminated = true
-      matched = nil
-
-      # Everything else that is not:
-      # - an ampersand
-      # - a pipe character
-      # - a semicolon
-      # - a single or double quote
-      # - a whitespace character
-      matched = true if scanner.skip(/[^&|;'"\s]+/)
-
-      # a single-quoted string
-      matched ||= if scanner.skip(SINGLE_QUOTE)
-        until scanner.skip(SINGLE_QUOTE)
-          if scanner.eos?
-            terminated = false
-            break
-          elsif scanner.skip(BACKSLASH)
-            scanner.getch
-          else
-            scanner.skip(/[^\\']+/)
-          end
-        end
-
-        true
-      end
-
-      # a double-quoted string
-      matched ||= if scanner.skip(DOUBLE_QUOTE)
-        until scanner.skip(DOUBLE_QUOTE)
-          if scanner.eos?
-            terminated = false
-            break
-          elsif scanner.skip(BACKSLASH)
-            scanner.getch
-          else
-            scanner.skip(/[^\\"]+/)
-          end
-        end
-
-        true
-      end
-
-      if matched
-        token_class = if terminated
-          Token::String
-        else
-          Token::UnterminatedString
-        end
-
-        return token_class.new(
-          source: line,
-          start_position: start_position,
-          end_position: scanner.charpos
-        )
-      end
-
-      raise UnreachableError, "#{start_position}: Unknown string parsing error"
-    end
-    private_class_method :scan_string_token
   end
 end
